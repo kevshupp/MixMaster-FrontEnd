3f3947cf89257dbb7f7d4b1c9b27dcead51e7ced
Kevin Castro <kcastrod@uft.edu>
2024-11-21 01:21:23 -0300

    actualizacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: AppComponent }, // Página principal (Home)
+  { path: 'signup', component: SignupComponent }, // Página de registro
+  { path: '**', redirectTo: '' }, // Ruta por defecto en caso de no coincidir con ninguna
+];
+
